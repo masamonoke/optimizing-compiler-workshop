85581b12641e2c197494cd15ea5a056376d2a5b6
Mark Rudakov <mrudakov@code-industry.net>
2026-04-30 14:46:11 +0300

    run as test, refactor

--- simple_constant_folding.py
+++ simple_constant_folding.py
@@ -2,9 +2,10 @@
 
 from enum import Enum
 from typing import Any, Callable, Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from copy import deepcopy
 import operator
+import unittest
 
 
 class LatticeValueKind(Enum):
@@ -41,7 +42,16 @@ class LatticeValue:
 
         return LatticeValue(LatticeValueKind.TOP)
 
-    def _op(self, other: LatticeValue | float | int, op):
+    def _op(self, other: LatticeValue | float | int, op: Callable) -> LatticeValue:
+        # pylint: disable=line-too-long
+        """
+        Executes 'self op other'
+        The rules are following:
+        1. if self or other is TOP then result of op is TOP
+        2. if self or other is BOTTOM then result of op it BOTTOM
+        3. if other is numeric then result is new LatticeValue(const=self.const + other), self must be CONST
+        4. if other is CONST then result if new LatticeValue(const=self.const + other.const), self must be CONST
+        """
         if self.kind in (LatticeValueKind.BOTTOM, LatticeValueKind.TOP):
             return LatticeValue(kind=self.kind)
 
@@ -142,20 +152,17 @@ def join(m_in: LatticeVector, pred: LatticeVector) -> LatticeVector:
     """
     return LatticeVector(m_in.n, [a.join(b) for a, b in zip(m_in, pred)])
 
-# pylint: disable=too-few-public-methods
 
-
+@dataclass(eq=False)
 class BasicBlock:
     """
-    Basic block which abstracts execution of a code
+    Abstracts execution basic block
     """
-
-    def __init__(self, name: str) -> None:
-        self.vars: dict[str, LatticeValue] = {}
-        self.preds: list[BasicBlock] = []
-        self.transfer_function: Optional[Callable[[
-            LatticeVector], LatticeVector]] = None
-        self.name: str = name
+    name: str
+    vars: dict[str, "LatticeValue"] = field(default_factory=dict)
+    preds: list["BasicBlock"] = field(default_factory=list)
+    transfer_function: Optional[Callable[[
+        "LatticeVector"], "LatticeVector"]] = None
 
     def __str__(self) -> str:
         return f"BasicBlock: {self.vars}, preds={[bb.name for bb in self.preds]}"
@@ -245,155 +252,160 @@ def propagate_const(scope: Scope, max_iter: int = 10000):
                 scope.replace_in_block(block, v, propagated_value)
 
 
-# TODO: set the testcase as test class
+class ConstPropagationTest(unittest.TestCase):
+    """
+    Tests constant propagation algorithm correctness
+    """
 
-# hir
-# BB1:
-# i = 0
-# x = 1
-# y = 1
-#
-# BB2:
-# x = x + 1
-# i = i + 1
-# y = y * y
-# if y < 10 goto BB2; else goto BB3
-#
-# BB3:
-# p = x + y
+    def setUp(self) -> None:
+        self.scope = self.init_scope()
 
-# передаточные функции для hir
-# FB1([i, x, y, p]) = [0, 1, 1, p]
-# FB2([i, x, y, p]) = [i + 1, x * i, y * y, p]
-# FB3([i, x, y, p]) = [i, x, y, x + y]
+    def test_const_propagation(self):
+        """
+        Tests book's hir listing
+        """
+        before = str(self.scope)
+        propagate_const(self.scope)
+        after = str(self.scope)
 
-# дерево переходов
-# BB1
-# └── BB2
-#     ├── BB3
-#     └── BB2
+        print(before, '\n')
+        print(after)
 
-def init_bb1() -> BasicBlock:
-    """Inits B1"""
-    bb = BasicBlock(name="BB1")
+        self.assertTrue(before == "BB1: 011u\nBB2: 101u\nBB3: 1011")
+        self.assertTrue(after == "BB1: 011u\nBB2: --1u\nBB3: --1-")
 
-    bb.vars = {
-        "i": LatticeValue(LatticeValueKind.CONST, 0),
-        "x": LatticeValue(LatticeValueKind.CONST, 1),
-        "y": LatticeValue(LatticeValueKind.CONST, 1),
-        "p": LatticeValue(LatticeValueKind.BOTTOM)
-    }
+    # BB1:
+    # i = 0
+    # x = 1
+    # y = 1
+    #
+    # BB2:
+    # x = x + 1
+    # i = i + 1
+    # y = y * y
+    # if y < 10 goto BB2; else goto BB3
+    #
+    # BB3:
+    # p = x + y
 
-    def transfer_function(lattice: LatticeVector) -> LatticeVector:
-        trans_lattice = deepcopy(lattice)
-        trans_lattice[0] = bb.vars["i"]
-        trans_lattice[1] = bb.vars["x"]
-        trans_lattice[2] = bb.vars["y"]
-        trans_lattice[3] = bb.vars["p"]
-        return trans_lattice
+    # передаточные функции для hir
+    # FB1([i, x, y, p]) = [0, 1, 1, p]
+    # FB2([i, x, y, p]) = [i + 1, x * i, y * y, p]
+    # FB3([i, x, y, p]) = [i, x, y, x + y]
 
-    bb.transfer_function = transfer_function
+    # дерево переходов
+    # BB1
+    # └── BB2
+    #     ├── BB3
+    #     └── BB2
 
-    return bb
+    def init_scope(self):
+        """Inits scope with B1, B2 and B3 basic blocks"""
+        scope = Scope()
+        bb1 = self.init_bb1()
+        bb2 = self.init_bb2(bb1)
+        bb3 = self.init_bb3(bb2)
+        scope.all_blocks.extend([bb1, bb2, bb3])
+        for b in scope.all_blocks:
+            assert b.vars is not None
+            assert isinstance(b.vars, dict)
+            for var, _ in b.vars.items():
+                scope.define_var(var)
 
+        return scope
 
-def init_bb2(bb1: BasicBlock) -> BasicBlock:
-    """Inits B2, using B1 as its parent"""
-    bb = BasicBlock(name="BB2")
+    def init_bb1(self) -> BasicBlock:
+        """Inits B1"""
+        bb = BasicBlock(name="BB1")
 
-    def transfer_i(i: LatticeValue):
-        return i + 1
+        bb.vars = {
+            "i": LatticeValue(LatticeValueKind.CONST, 0),
+            "x": LatticeValue(LatticeValueKind.CONST, 1),
+            "y": LatticeValue(LatticeValueKind.CONST, 1),
+            "p": LatticeValue(LatticeValueKind.BOTTOM)
+        }
 
-    def transfer_x(x: LatticeValue, i: LatticeValue):
-        return x * i
+        def transfer_function(lattice: LatticeVector) -> LatticeVector:
+            trans_lattice = deepcopy(lattice)
+            trans_lattice[0] = bb.vars["i"]
+            trans_lattice[1] = bb.vars["x"]
+            trans_lattice[2] = bb.vars["y"]
+            trans_lattice[3] = bb.vars["p"]
+            return trans_lattice
 
-    def transfer_y(y: LatticeValue):
-        return y * y
+        bb.transfer_function = transfer_function
 
-    def transfer_p(p: LatticeValue):
-        return p
+        return bb
 
-    bb.vars["i"] = transfer_i(bb1.vars["i"])
-    bb.vars["x"] = transfer_x(bb1.vars["x"], bb1.vars["i"])
-    bb.vars["y"] = transfer_y(bb1.vars["y"])
-    bb.vars["p"] = transfer_p(bb1.vars["p"])
+    def init_bb2(self, bb1: BasicBlock) -> BasicBlock:
+        """Inits B2, using B1 as its parent"""
+        bb = BasicBlock(name="BB2")
 
-    def transfer_function(lattice: LatticeVector) -> LatticeVector:
-        transfered = LatticeVector(copy=lattice)
-        transfered[0] = transfer_i(lattice[0])
-        transfered[1] = transfer_x(lattice[1], lattice[0])
-        transfered[2] = transfer_y(lattice[2])
-        transfered[3] = transfer_p(lattice[3])
+        def transfer_i(i: LatticeValue):
+            return i + 1
 
-        return transfered
+        def transfer_x(x: LatticeValue, i: LatticeValue):
+            return x * i
 
-    bb.transfer_function = transfer_function
+        def transfer_y(y: LatticeValue):
+            return y * y
 
-    bb.preds.append(bb1)
-    bb.preds.append(bb)  # loop
+        def transfer_p(p: LatticeValue):
+            return p
 
-    return bb
+        bb.vars["i"] = transfer_i(bb1.vars["i"])
+        bb.vars["x"] = transfer_x(bb1.vars["x"], bb1.vars["i"])
+        bb.vars["y"] = transfer_y(bb1.vars["y"])
+        bb.vars["p"] = transfer_p(bb1.vars["p"])
 
+        def transfer_function(lattice: LatticeVector) -> LatticeVector:
+            transfered = LatticeVector(copy=lattice)
+            transfered[0] = transfer_i(lattice[0])
+            transfered[1] = transfer_x(lattice[1], lattice[0])
+            transfered[2] = transfer_y(lattice[2])
+            transfered[3] = transfer_p(lattice[3])
 
-def init_bb3(bb2: BasicBlock) -> BasicBlock:
-    """Inits B3 using B2 as its parent"""
-    bb = BasicBlock(name="BB3")
+            return transfered
 
-    def transfer_i(i: LatticeValue):
-        return i
+        bb.transfer_function = transfer_function
 
-    def transfer_x(x: LatticeValue):
-        return x
+        bb.preds.append(bb1)
+        bb.preds.append(bb)  # loop
 
-    def transfer_y(y: LatticeValue):
-        return y
+        return bb
 
-    def transfer_p(x: LatticeValue, y: LatticeValue):
-        return x + y
+    def init_bb3(self, bb2: BasicBlock) -> BasicBlock:
+        """Inits B3 using B2 as its parent"""
+        bb = BasicBlock(name="BB3")
 
-    bb.vars["i"] = transfer_i(bb2.vars["i"])
-    bb.vars["x"] = transfer_x(bb2.vars["x"])
-    bb.vars["y"] = transfer_y(bb2.vars["y"])
-    bb.vars["p"] = transfer_p(bb2.vars["x"], bb2.vars["y"])
+        def transfer_i(i: LatticeValue):
+            return i
 
-    def transfer_function(lattice: LatticeVector) -> LatticeVector:
-        transfered = LatticeVector(copy=lattice)
-        transfered[3] = lattice[1] + lattice[2]
+        def transfer_x(x: LatticeValue):
+            return x
 
-        return transfered
+        def transfer_y(y: LatticeValue):
+            return y
 
-    bb.transfer_function = transfer_function
-    bb.preds.append(bb2)
+        def transfer_p(x: LatticeValue, y: LatticeValue):
+            return x + y
 
-    return bb
+        bb.vars["i"] = transfer_i(bb2.vars["i"])
+        bb.vars["x"] = transfer_x(bb2.vars["x"])
+        bb.vars["y"] = transfer_y(bb2.vars["y"])
+        bb.vars["p"] = transfer_p(bb2.vars["x"], bb2.vars["y"])
 
+        def transfer_function(lattice: LatticeVector) -> LatticeVector:
+            transfered = LatticeVector(copy=lattice)
+            transfered[3] = lattice[1] + lattice[2]
 
-def init_scope() -> Scope:
-    """Inits scope with B1, B2 and B3 basic blocks"""
-    scope = Scope()
-    bb1 = init_bb1()
-    bb2 = init_bb2(bb1)
-    bb3 = init_bb3(bb2)
-    scope.all_blocks.extend([bb1, bb2, bb3])
-    for b in scope.all_blocks:
-        assert b.vars is not None
-        assert isinstance(b.vars, dict)
-        for var, _ in b.vars.items():
-            scope.define_var(var)
+            return transfered
 
-    return scope
+        bb.transfer_function = transfer_function
+        bb.preds.append(bb2)
 
-
-def main():
-    """Executes the test case"""
-    scope = init_scope()
-
-    print(scope, '\n')
-
-    propagate_const(scope)
-
-    print(scope)
+        return bb
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
